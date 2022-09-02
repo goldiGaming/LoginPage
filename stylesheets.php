fe4604b3f33312feb6f0f0096a67649a51001e89
goldiGaming <112711338+goldiGaming@users.noreply.github.com>
2022-09-02 16:57:41 +0200

    Update stylesheets.php

--- stylesheets.php
+++ stylesheets.php
@@ -7,7 +7,7 @@
         <!-- Stylesheets -->
         
         <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="fontawesome-5.15.1/css/all.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
         
